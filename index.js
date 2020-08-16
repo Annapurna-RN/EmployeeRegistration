05994e654bf40475441d74a056d348fdf17e1f2a
Annapurna-RN <68640201+Annapurna-RN@users.noreply.github.com>
2020-08-16 21:53:18 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
- function saveName(){
-	 
-	  var  key = "name";    
-        var value = document.getElementById('putName').value;  
-		 localStorage.setItem(key,value);
-		
-		 document.getElementById('i1').innerHTML=value;
-}
