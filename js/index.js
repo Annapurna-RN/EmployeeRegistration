2cfd84f841897747336796942fb368d75e5decb2
Annapurna-RN <68640201+Annapurna-RN@users.noreply.github.com>
2020-08-18 19:34:19 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
- function saveName(){
-	 
-	  var  key = "name";    
-        var value = document.getElementById('putName').value;  
-		 localStorage.setItem(key,value);
-		
-		 document.getElementById('i1').innerHTML=value;
+ function saveName()
+ {
+	var table=document.getElementById("table");
+	var  name = "Name";    
+	
+	var employeeName = document.getElementById('putName').value; 
+	
+	localStorage.setItem(name,JSON.stringify(employeeName));
+	
+	var row= table.insertRow();
+	 var cell=row.insertCell(0);
+	 cell.innerHTML=employeeName;
+	
 }
